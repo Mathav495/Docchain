f65bd9179145dcfd8a26cd0b8d4b3a985cb02b9e
GOWTHAMAN R <pudukkottai>
2022-11-22 19:02:53 +0530

    nav bar created

--- client/src/Pages/Dashboard.svelte
+++ client/src/Pages/Dashboard.svelte
@@ -35,4 +35,46 @@
       </div>
     </div>
   </div>
+  <div class="flex flex-row">
+    <div class="flex w-1/4 px-5 py-5 lg:w-1/6">
+      <nav class="flex-1 space-y-3 pb-4">
+        <a
+          href="Dash"
+          class="flex items-center gap-2 rounded-md border-red-700 px-2 py-2 text-sm font-medium text-white hover:border-l-2">
+          <svg
+            class="h-6 w-6 flex-shrink-0"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            aria-hidden="true">
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
+          </svg>
+          Dashboard
+        </a>
+        <a
+          href="publish"
+          class="flex items-center gap-2 rounded-md border-red-700 px-2 py-2 text-sm font-medium text-white hover:border-l-2">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            class="h-6 w-6 flex-shrink-0">
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M2.25 13.5h3.86a2.25 2.25 0 012.012 1.244l.256.512a2.25 2.25 0 002.013 1.244h3.218a2.25 2.25 0 002.013-1.244l.256-.512a2.25 2.25 0 012.013-1.244h3.859m-19.5.338V18a2.25 2.25 0 002.25 2.25h15A2.25 2.25 0 0021.75 18v-4.162c0-.224-.034-.447-.1-.661L19.24 5.338a2.25 2.25 0 00-2.15-1.588H6.911a2.25 2.25 0 00-2.15 1.588L2.35 13.177a2.25 2.25 0 00-.1.661z" />
+          </svg>
+          Publish Document
+        </a>
+      </nav>
+    </div>
+    <div class="flex w-3/4 px-5 py-5 lg:w-5/6">content</div>
+  </div>
 </div>
