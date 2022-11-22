ef355069ae4e0d241007b85fe260b987b381c384
GOWTHAMAN R <pudukkottai>
2022-11-22 20:53:48 +0530

    header file created

--- client/src/Pages/Publishdoc.svelte
+++ client/src/Pages/Publishdoc.svelte
@@ -1,41 +1,10 @@
 <script>
+import Header from "../components/Header.svelte"
 import Navbar from "../components/Navbar.svelte"
 </script>
 
 <div class="h-screen w-screen bg-black text-gray-300">
-  <div class="flex flex-row items-center shadow-sm shadow-red-700">
-    <div class="flex w-1/4  cursor-pointer justify-start px-5 py-5 lg:w-1/6">
-      <img src="assets\icon2.png" alt="icon" class="animate-pulse" />
-      <h1 class="fond-bold text-xl">Docchain.io</h1>
-    </div>
-    <div class="flex w-3/4  items-center justify-between px-5 py-5 lg:w-5/6">
-      <div class="flex flex-row items-center justify-end gap-2">
-        <img
-          src="assets\profile.jpg"
-          alt="profile"
-          class="h-10 w-10 cursor-pointer rounded-md" />
-        <div class="flex flex-col">
-          <h1 class="text-sm font-thin">Welcome,</h1>
-          <p>User1</p>
-        </div>
-      </div>
-      <div
-        class="flex h-10 w-10 items-center justify-center rounded-lg hover:bg-red-700 hover:text-white">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke-width="1.5"
-          stroke="currentColor"
-          class="flex h-6 w-6">
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15M12 9l-3 3m0 0l3 3m-3-3h12.75" />
-        </svg>
-      </div>
-    </div>
-  </div>
+  <Header />
   <div class="flex flex-row">
     <Navbar />
     <div class="flex w-3/4 px-5 py-5 lg:w-5/6">Publishdoc</div>
